b13222cc467eb7ddfc5689842eed817ffc20cc97
rockroman <rock.rakic@gmail.com>
2022-08-18 22:39:52 +0000

    Display addition question to the user

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,25 +8,32 @@ document.addEventListener('DOMContentLoaded',()=>{
                 alert('you clicked submit!');
             }else{
                 let gameType=this.getAttribute('data-type');
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
                
             }
 
         });
 
     }
+    runGame('addition');
 
 })
 /**
  * main game loop called when script is loaded
  * after users answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     let num1=Math.floor(Math.random() * 25)+ 1;
     let num2=Math.floor(Math.random() * 25)+ 1;
+    if(gameType==='addition'){
+        displayAdditionQuestion(num1,num2)
+    }else{
+        alert(`unknown game type:${gameType}`);
+        throw`unknown game type:${gameType}.aborting!`
+    }
 
 };
-runGame();
+
 function  checkAnswer(){
    
 };
@@ -43,7 +50,10 @@ function incrementWrongAnswer(){
 
 };
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1,operand2){
+    document.getElementById('operand1').textContent =operand1;
+    document.getElementById('operand2').textContent =operand2;
+    document.getElementById('operator').textContent ='+';
 
 };
 
