caff7398683a7d2fe623a8ab0bb2a2e403ace1a2
rockroman <rock.rakic@gmail.com>
2022-08-21 20:25:15 +0000

    add multiplication game code

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,9 @@ function runGame(gameType){
     let num2=Math.floor(Math.random() * 25)+ 1;
     if(gameType==='addition'){
         displayAdditionQuestion(num1,num2)
-    }else{
+    }else if(gameType==='multiply'){
+        displayMultiplyQuestion(num1,num2)
+    } else{
         alert(`unknown game type:${gameType}`);
         throw`unknown game type:${gameType}.aborting!`
     }
@@ -63,7 +65,9 @@ function calculateCorrectAnswer(){
 
     if(operator==='+'){
         return[operand1+operand2,'addition']
-    }else{
+    }else if(operator==='x'){
+        return[operand1 * operand2,'multiply']
+    } else{
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}.aborting!`
     }
@@ -99,6 +103,9 @@ function displaySubtractQuestion(){
 
 };
 
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion(operand1,operand2){
+    document.getElementById('operand1').textContent =operand1;
+    document.getElementById('operand2').textContent =operand2;
+    document.getElementById('operator').textContent ='x';
 
 };
